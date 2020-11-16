2b075d5975ca0baf3d96f366545e5c12369274a7
西川大斗 <nishikawahiroto@nh-mac.local>
2020-11-16 11:45:35 +0900

    プログラム修正

--- Carpark.java
+++ Carpark.java
@@ -1,5 +1,11 @@
 public class Carpark{
   public static void main(String[] args) {
+    System.out.println("駐車場のスペースを入力してください");
+    if(args.length != 1){
+      System.out.println("入力を1つにして下さい");
+      System.exit(1);
+    }
+    
     CarparkControl control = new CarparkControl();
     control.CarparkControl(Integer.parseInt(args[0]));
     Arrivals thread_arrive = new Arrivals();
