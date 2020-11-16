347984ed08956385e5bf7f764e9920a912543011
西川大斗 <nishikawahiroto@nh-mac.local>
2020-11-16 11:40:37 +0900

    プログラム修正

--- Carpark.java
+++ Carpark.java
@@ -1,7 +1,7 @@
 public class Carpark{
   public static void main(String[] args) {
-    CarparlkControl control = new CarparkControl();
-    control.CarparkControl(Integer.parseInt(args));
+    CarparkControl control = new CarparkControl();
+    control.CarparkControl(Integer.parseInt(args[0]));
     Arrivals thread_arrive = new Arrivals();
     Departures thread_depart = new Departures();
     thread_arrive.start();
